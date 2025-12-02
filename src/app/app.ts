0c6c8593768312de2f33459ac710c85bd2711761
Arif <mdarifulislam6898@gmail.com>
2025-12-02 12:27:17 +0600

    create the component and connect the component with the main component app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Roles } from './components/roles/roles';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Roles],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/roles/roles.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [],
+  templateUrl: './roles.html',
+  styleUrl: './roles.css',
+})
+export class Roles {
+
+}

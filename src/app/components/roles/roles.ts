3327b01478c7720332cebc4e9f4fbaa3574f3da2
Arif <mdarifulislam6898@gmail.com>
2025-12-03 13:04:53 +0600

    use ngFor directives and make the api call to fetch the data from a get api and use the data which is get from api call

--- src/app/components/roles/roles.ts
+++ src/app/components/roles/roles.ts
@@ -1,29 +1,56 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../Model/Interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './roles.html',
   styleUrl: './roles.css',
 })
-export class Roles {
-  firstName: string = "Angular Learn";
-  angularVersion = "version-24";
-  version: number = 24;
-  isActive: boolean = false;
-  currentDate: Date = new Date();
-  inputType: string = "radio";
-  slectedState: string = '';
+export class Roles implements OnInit {
+  
+  roleList : IRole [] = [];
+  http = inject(HttpClient);
 
-  showWelcomeAlert()
-  {
-    alert("Welcome to Angular learning project");
+  ngOnInit(): void {
+    this.getAllRoles();
   }
 
-  showMessage(message: string)
+  getAllRoles()
   {
-    alert(message);
+    this.http.get("/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
+      this.roleList = res.data;
+    })
   }
+
+
+
+
+
+
+
+
+
+
+  // firstName: string = "Angular Learn";
+  // angularVersion = "version-24";
+  // version: number = 24;
+  // isActive: boolean = false;
+  // currentDate: Date = new Date();
+  // inputType: string = "radio";
+  // slectedState: string = '';
+
+  // showWelcomeAlert()
+  // {
+  //   alert("Welcome to Angular learning project");
+  // }
+
+  // showMessage(message: string)
+  // {
+  //   alert(message);
+  // }
 }

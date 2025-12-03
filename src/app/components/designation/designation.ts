55f67137f3ec883d8051d148f41f86b8adb159c6
Arif <mdarifulislam6898@gmail.com>
2025-12-03 18:58:55 +0600

    use processing before the data load

--- src/app/components/designation/designation.ts
+++ src/app/components/designation/designation.ts
@@ -1,24 +1,28 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Master } from '../../Services/master';
 import { APIResponseModal, IDesignation } from '../../Model/Interface/role';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-designation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './designation.html',
   styleUrl: './designation.css',
 })
 export class Designation implements OnInit {
   designationList : IDesignation [] = [];
+  isLoader: boolean = true;
   masterService = inject(Master);
   
   ngOnInit(): void {
     this.masterService.getDesignations().subscribe((result:APIResponseModal)=>{
       this.designationList = result.data;
+      this.isLoader = false;
     }, error=>{
       alert("API error / Network Down");
+      this.isLoader = false;
     })
   }
 }

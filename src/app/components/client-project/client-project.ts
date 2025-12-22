5845ad7504e4449700787a772e848cca36f43ed0
Arif <mdarifulislam6898@gmail.com>
2025-12-22 07:07:52 +0600

    make reactive form for client-project component

---
+++ src/app/components/client-project/client-project.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-client-project',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './client-project.html',
+  styleUrl: './client-project.css',
+})
+export class ClientProject {
+  projectForm: FormGroup = new FormGroup({
+    clientProjectId: new FormControl(0),
+    projectName : new FormControl(""),
+    startDate : new FormControl(""),
+    expectedEndDate : new FormControl(""),
+    leadByEmpId : new FormControl(""),
+    completedDate : new FormControl(""),
+    contactPerson : new FormControl(""),
+    contactPersonContactNo : new FormControl(""),
+    totalEmpWorking : new FormControl(""),
+    projectCost : new FormControl(""),
+    projectDetails : new FormControl(""),
+    contactPersonEmailId : new FormControl(""),
+    clientId : new FormControl(""),
+  })
+}
